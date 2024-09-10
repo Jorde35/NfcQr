13ae5ef49d2ca353804336a24d1d521f369d6407
Jorge Javier Ignacio Campillay Peña <jorge.campillay-practica@global.komatsu>
2024-09-10 17:17:03 -0300

    Agregar archivos de proyecto.

---
+++ NfcQr/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NfcQr.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
